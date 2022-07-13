2eddb12c463e93ffe46ee9db43b76202f2db74ea
SimpliLearn Github <42lukastefanovic@gmail.com>
2022-07-13 13:13:00 +0200

    initial changes to FreshVotes app

---
+++ FreshVotes/src/main/java/com/freshvotes/DashboardController.java
@@ -0,0 +1,19 @@
+package com.freshvotes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String rootView() {
+		
+		// it will return html file called index.html
+		// root page of our application
+		return "index";
+	}
+	
+	
+}
