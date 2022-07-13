42c3840dc22dbacf14285d835075ae7b620e6448
SimpliLearn Github <42lukastefanovic@gmail.com>
2022-07-13 17:23:33 +0200

    adding password encoder

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,60 +1,48 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// we want to make less secure spring-boot-starter-security dependency by configuration using java
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+//we want to make less secure spring-boot-starter-security dependency by configuration using java
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		// method asks who are you and prove it
-		// tell spring security where are usenames and passwords
+		// tell spring security where are usernames and passwords
 		// by building a database of usernames and passwords
-		auth.inMemoryAuthentication()
-			.withUser("jal3lsl23s")
-			.password("3lj289sjz297sl249sljj239sljf")
-			.roles("USER"); // authentication
+		auth.inMemoryAuthentication().passwordEncoder(getPasswordEncoder()).withUser("mtopkovic@mailme.com")
+				.password(getPasswordEncoder().encode("asdfasdf")).roles("USER"); // authentication
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
 		// when you authenticate your self what access should i gave you
-		// are you like administrator or some end point user client 
-		http.csrf().disable()
-			.authorizeRequests()
-			//.antMatchers("/index").permitAll() // only the index file is allowed to be seen
-			.antMatchers("/").permitAll() //  root of the web application is allowed to be seen by anyone only the index file is allowed to be seen
-			.anyRequest().hasRole("USER").and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll() // only authorized people should see login page
-				.and()
-			.logout()
-				.logoutUrl("/logout")
-				.permitAll();
-		
+		// are you like administrator or some end point user client
+		http.csrf().disable().authorizeRequests()
+				// .antMatchers("/index").permitAll() // only the index file is allowed to be
+				// seen
+				.antMatchers("/").permitAll() // root of the web application is allowed to be seen by anyone only the
+												// index file is allowed to be seen
+
+				.anyRequest().hasRole("USER").and().formLogin().loginPage("/login").permitAll().and().logout()
+				.logoutUrl("/logout").permitAll();
+
 	}
-	
-	
+
 	// we need a mapping for login.html page
 	// actually we need a listener
-	
-	
 
-	
-	
 }
-
-
-
-
-
-
-
-
--- FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -1,12 +1,20 @@
 package com.freshvotes.web;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class DashboardController {
 	
+
+	
+	
+	// @RequestMapping annotation same as @GetMapping
+	//@RequestMapping(value="/", method=RequestMethod.GET)
 	@GetMapping("/")
 	public String rootView() {
 		
@@ -14,6 +22,12 @@ public class DashboardController {
 		// root page of our application
 		
 		return "index";
+	}
+	
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
 	}
 	
 	
