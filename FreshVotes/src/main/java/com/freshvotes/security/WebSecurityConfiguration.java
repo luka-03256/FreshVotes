6cb9e68461f17912758ba53dc5e397f2960ea8ea
SimpliLearn Github <42lukastefanovic@gmail.com>
2022-07-14 15:16:31 +0200

    enabling CSRF protection

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.freshvotes.security;
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -23,26 +25,55 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// method asks who are you and prove it
 		// tell spring security where are usernames and passwords
 		// by building a database of usernames and passwords
-		auth.inMemoryAuthentication().passwordEncoder(getPasswordEncoder()).withUser("mtopkovic@mailme.com")
-				.password(getPasswordEncoder().encode("asdfasdf")).roles("USER"); // authentication
+		auth.inMemoryAuthentication()
+			.passwordEncoder(getPasswordEncoder())
+			.withUser("mtopkovic@mail.com")
+			.password(getPasswordEncoder().encode("asdfasdf132"))
+			.roles("USER");
+		// authentication
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// when you authenticate your self what access should i gave you
 		// are you like administrator or some end point user client
-		http.csrf().disable().authorizeRequests()
-				// .antMatchers("/index").permitAll() // only the index file is allowed to be
-				// seen
-				.antMatchers("/").permitAll() // root of the web application is allowed to be seen by anyone only the
-												// index file is allowed to be seen
+		
+		
+		// .antMatchers("/index").permitAll() // only the index file is allowed to be
+		// seen
 
-				.anyRequest().hasRole("USER").and().formLogin().loginPage("/login").permitAll().and().logout()
-				.logoutUrl("/logout").permitAll();
+		// root of the web application is allowed to be seen by anyone only the
+		// index file is allowed to be seen
+		http
+		.authorizeRequests()
+				.antMatchers("/").permitAll() 
+				.anyRequest().hasRole("USER")
+				.and()
+			.formLogin()
+					.loginPage("/login")
+					.defaultSuccessUrl("/dashboard")
+					.permitAll()
+					.and()
+			.logout()
+					.logoutUrl("/logout")
+					.permitAll();
 
 	}
 
 	// we need a mapping for login.html page
 	// actually we need a listener
 
+	
+	
+	// csrf - stands for () => cross site request forgery
+	// hacker knows when you are logged in your bank account
+	// and they send you their fake page (redirect you to their page)
+	// that page can actually post data to your bank on your behalf
+	
+	// in order to protect against this kind of attacks
+	// you could have csrf token which represents random number alpha-numeric value
+	// basically anything that isn't a get request will be secured
+	
+	
+	
 }
--- FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -3,8 +3,7 @@ package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
