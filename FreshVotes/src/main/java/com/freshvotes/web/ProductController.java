189021c8cd6637684a303c006966b2f68e9cb232
Luka <42lukastefanovic@gmail.com>
2022-07-26 15:38:43 +0200

    building page to display list of all products fetched from db

--- FreshVotes/src/main/java/com/freshvotes/web/ProductController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/ProductController.java
@@ -1,6 +1,7 @@
 package com.freshvotes.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.freshvotes.domain.Product;
 import com.freshvotes.domain.User;
 import com.freshvotes.repositories.ProductRepository;
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ProductController {
@@ -17,19 +21,43 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepo;
 
-	@GetMapping(value = "/products")
+	@GetMapping(value = "/product")
 	public String getProducts(ModelMap model) {
-		return "products";
+		return "product";
 	}
 
 	// {productId} -> open up the page with given productId and display information
 	// fetched from database
-	@GetMapping(value="/products/{productId}")
-	public String getProduct(@PathVariable Long productId) {
-		return "products";
+	@GetMapping(value = "/product/{productId}")
+	public String getProduct(@PathVariable Long productId, ModelMap model, HttpServletResponse response)
+			throws IOException {
+		Optional<Product> productOpt = productRepo.findById(productId);
+
+		if (productOpt.isPresent()) {
+			Product product = productOpt.get();
+			model.put("product", product);
+
+		} else {
+			Product product = productOpt.get();
+			model.put("product", product);
+
+			// if we don't find productOpt we explicitly set status
+			// 404 not found
+			response.sendError(HttpStatus.NOT_FOUND.value(), "Product with id " + productId + " wasn't found");
+			return "product";
+		}
+
+		return "product";
 	}
 
-	@PostMapping(value = "/products")
+	@PostMapping(value = "/product/{productId}")
+	public String saveProduct(@PathVariable Long productId, Product product) {
+		System.out.println(product);
+		product = productRepo.save(product);
+		return "redirect:/product/" + product.getId();
+	}
+
+	@PostMapping(value = "/product")
 	public String createProduct(@AuthenticationPrincipal User user) {
 		Product product = new Product();
 
@@ -39,7 +67,7 @@ public class ProductController {
 
 		// redirect us to page with current product id
 		// fetched from our database
-		return "redirect:/products/" + product.getId();
+		return "redirect:/product/" + product.getId();
 	}
 
 }
