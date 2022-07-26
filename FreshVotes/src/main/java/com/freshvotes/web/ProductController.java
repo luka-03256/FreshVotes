f474f78d40f9d63993ba5cdfe9733473a81ee778
Luka <42lukastefanovic@gmail.com>
2022-07-26 16:02:24 +0200

    lksj02jlksjlksj02jlksj

--- FreshVotes/src/main/java/com/freshvotes/web/ProductController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/ProductController.java
@@ -1,5 +1,9 @@
 package com.freshvotes.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,12 +12,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import com.freshvotes.domain.Product;
 import com.freshvotes.domain.User;
 import com.freshvotes.repositories.ProductRepository;
-import java.io.IOException;
-import java.util.Optional;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ProductController {
